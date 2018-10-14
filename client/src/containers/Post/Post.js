ee39ff1617b21d26cd6cf5ce71c72e644808bc08
Ghassanooooo <35297725+Ghassanooooo@users.noreply.github.com>
2018-10-14 21:38:59 +0200

    move the layout

--- client/src/containers/Post/Post.js
+++ client/src/containers/Post/Post.js
@@ -1,15 +1,30 @@
 import React, { Component } from "react";
 import Post from "../../components/post/post";
-import Layout from "../../HOC/layout/Layout";
+import { withStyles } from "@material-ui/core/styles";
 
+const styles = theme => ({
+  layout: {
+    width: "auto",
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(900 + theme.spacing.unit * 3 * 2)]: {
+      width: 900,
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
+  }
+});
 class post extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <Layout>
-        <Post />
-      </Layout>
+      <div className="Layout">
+        <main className={classes.layout}>
+          <Post />
+        </main>
+      </div>
     );
   }
 }
 
-export default post;
+export default withStyles(styles)(post);
