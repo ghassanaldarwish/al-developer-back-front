6deb130be94b1988a37f9ca2464cc9e099495e27
Ghassanooooo <35297725+Ghassanooooo@users.noreply.github.com>
2018-10-30 18:11:52 +0100

    test??

--- models/Users.js
+++ models/Users.js
@@ -18,6 +18,10 @@ const UserSchema = new Schema({
     type: String,
     required: true
   },
+  admin: {
+    type: Boolean,
+    default: false
+  },
   date: {
     type: Date,
     default: Date.now
