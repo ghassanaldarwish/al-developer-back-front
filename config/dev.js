fa1ff1df37887ef9c85460957fbf8d9287ec39a5
Ghassan Aldarwish <ghassanaldarwish@yahoo.com>
2018-10-30 14:50:25 +0100

    Delete dev.js

--- config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  googleClientID:
-    "135371707024-8plruj06ss4jkfiebmijbip8u3ap1cq5.apps.googleusercontent.com",
-  googleClientSecret: "80jYr8GC3F0iD2CniKPKI799",
-  mongoURI:
-    "mongodb://ghassan:Tntangeltnt89@ds123963.mlab.com:23963/aldeveloper-dev",
-  cookieKey: "qwertzuiopiewi884i849fvffoofksps",
-  authRedirect: "http://localhost:3000"
-};
